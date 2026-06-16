7512a3d9400dd19537aca678f969d89f9a2e2613
20muskm <20muskm@A13-14-25>
2026-06-16 10:10:51 +0100

    Re-added changes that were lost

--- DatabaseServer/Program.cs
+++ DatabaseServer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Data.Sqlite;
 using System.Runtime.CompilerServices;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to the database server");
@@ -71,45 +72,57 @@ while (running)
 
             int balance = 0;
 
-            Console.WriteLine($"{id,5}|{firstname,15}|{lastname,15}|{username,15}:{password,15}");
+            Console.WriteLine($"{id,3}|{firstname,15}|{lastname,15}|{username,15}|{password,15}");
 
             // get list of money received
-            string sql = $"SELECT * FROM Payment WHERE RecipientID={id}";
+            string sql = $"SELECT Payment.*, Person.FirstName, Person.LastName FROM Payment " +
+                "JOIN Person ON Payment.RecipientID = Person.PersonID " +
+                $"WHERE GiverID={id}";
             SqliteCommand transactions = c.CreateCommand();
             transactions.CommandText = sql;
             SqliteDataReader rTransactions = transactions.ExecuteReader();
-            Console.WriteLine($"Payments received:");
+            Console.WriteLine($"\nPayments received:");
             while(rTransactions.Read())
             {
                 int PaymentID = rTransactions.GetInt32(0);
                 int GiverID = rTransactions.GetInt32(1);
                 int RecipientID = rTransactions.GetInt32(2);
                 int Amount = rTransactions.GetInt32(3);
-                balance += Amount;
-                string Description = rTransactions.GetString(4);
+                string Date = rTransactions.GetString(4);
+                string Description = rTransactions.GetString(5);
+                string FirstName = rTransactions.GetString(6);
+                string LastName = rTransactions.GetString(7);
+                balance -= Amount;
 
-                Console.WriteLine($"{PaymentID} - {GiverID} sent {RecipientID} £{Amount} for {Description}");
+                Console.WriteLine($"{PaymentID,3}| Sent £{Amount} to ({RecipientID}) {FirstName} {LastName} for {Description}, date: {Date}");
 
 
             }
-
-            sql = $"SELECT * FROM Payment WHERE RecipientID={id}";
+            // List of money sent
+            
+            sql = $"SELECT Payment.*, Person.FirstName, Person.LastName FROM Payment " +
+                "JOIN Person ON Payment.GiverID = Person.PersonID " +
+                $"WHERE RecipientID={id}";
             transactions = c.CreateCommand();
             transactions.CommandText = sql;
             rTransactions = transactions.ExecuteReader();
-            Console.WriteLine($"Payments sent:");
+            Console.WriteLine($"\nPayments sent:");
             while (rTransactions.Read())
             {
                 int PaymentID = rTransactions.GetInt32(0);
                 int GiverID = rTransactions.GetInt32(1);
                 int RecipientID = rTransactions.GetInt32(2);
                 int Amount = rTransactions.GetInt32(3);
-                balance -= Amount;
-                string Description = rTransactions.GetString(4);
-                Console.WriteLine($"{PaymentID,5}|{GiverID,5}|{RecipientID,5}|-£{Amount,5}|{Description,5}");
+                string Date = rTransactions.GetString(4);
+                string Description = rTransactions.GetString(5);
+                string FirstName = rTransactions.GetString(6);
+                string LastName = rTransactions.GetString(7);
+                balance += Amount;
+                Console.WriteLine($"{PaymentID,3}| Recieved £{Amount} from ({GiverID}) {FirstName} {LastName} for {Description}, date: {Date}");
+
             }
 
-            Console.WriteLine($"Total balance: £{balance}");
+            Console.WriteLine($"\nTotal balance: £{balance}\n");
 
         } else
         {
